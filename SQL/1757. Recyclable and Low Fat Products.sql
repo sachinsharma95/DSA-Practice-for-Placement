67d7af006cbe2d6b0303d43a6022d482cb3ca1b2
SACHIN SHARMA <85471127+sachinsharma95@users.noreply.github.com>
2023-08-12 01:11:37 +0530

    Create 1757. Recyclable and Low Fat Products.sql

---
+++ SQL/1757. Recyclable and Low Fat Products.sql
@@ -0,0 +1,9 @@
+Q 1757. Recyclable and Low Fat Products
+-- https://leetcode.com/problems/recyclable-and-low-fat-products/description/?envType=study-plan-v2&envId=top-sql-50
+
+
+-- Solution 
+
+-- # Write your MySQL query statement below
+select product_id from Products
+where low_fats="Y" and recyclable="Y";
